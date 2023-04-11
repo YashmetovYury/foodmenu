164dcd3de3a1df8d36b696797edd729b17354e9b
Yashmetov Yury <Yashmetov.Yury@gmail.com>
2023-04-11 23:23:18 +0300

    finish

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
+import image from "@astrojs/image"; // https://astro.build/config
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [image(), tailwind()],
+  site: 'https://yashmetovyury.github.io',
+  base: '/foodmenu'
+});
